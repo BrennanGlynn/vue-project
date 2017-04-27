a87fa072b82515553cfc50ba004fdcaffb17fca6
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-04-26 22:17:12 -0600

    Extracted store to its own file

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,9 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 
 import Router from 'vue-router'
+import store from '../store'
 
 Vue.use(Router)
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
-  state: {
-    count: 0
-  },
-  mutations: {
-    increment: state => state.count++,
-    decrement: state => state.count--
-  }
-})
 
 const child = {
   template: `
