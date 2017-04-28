0657ece7ba3dfd884070c9c22c0cb192e60dc78b
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-04-27 18:53:50 -0600

    Bug Fix

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,8 +11,8 @@ export default {
      </p>
      <h2>Double Count</h2>
      <p>{{ double }}</p>
-     <h2>Count Squared</h2>
-     <p>{{ squared }}</p>
+     <h2>Double Squared</h2>
+     <p>{{ doubleSquared }}</p>
      <h2>Multiplied by:</h2>
      <input v-model="variable" type="number">
      <br>
@@ -22,14 +22,14 @@ export default {
     </div>`,
   computed: {
     count () {
-      return this.$store.state.b.count
+      return this.$store.state.count
     },
     moduleACount () {
       return this.$store.state.a.count
     },
     ...mapGetters([
       'double',
-      'squared',
+      'doubleSquared',
       'multiply'
     ])
   },
--- src/store.js
+++ src/store.js
@@ -6,36 +6,45 @@ Vue.use(Vuex)
 const moduleA = {
   state: {
     count: 100
+  },
+  getters: {
+    countOfB: state => state.count,
+    sumOfBoth (state, getters, rootState) {
+      return state.count + rootState.count
+    }
   }
 }
 
 const moduleB = {
   state: {
     count: 0
+  }
+}
+
+export default new Vuex.Store({
+  state: {
+    count: 0
   },
-  mutations: {
-    increment: state => state.count++,
-    decrement: state => state.count--
+  modules: {
+    a: moduleA,
+    b: moduleB
+  },
+  getters: {
+    double: state => {
+      return state.count * 2
+    },
+    doubleSquared: (state, getters) => {
+      return getters.double * getters.double
+    },
+    multiply: state => x => state.count * x
   },
   actions: {
     increment (context) {
       context.commit('increment')
     }
   },
-  getters: {
-    double: state => {
-      return state.count * 2
-    },
-    squared: (state, getters) => {
-      return getters.double * 2
-    },
-    multiply: state => x => state.count * x
-  }
-}
-
-export default new Vuex.Store({
-  modules: {
-    a: moduleA,
-    b: moduleB
+  mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--
   }
 })
